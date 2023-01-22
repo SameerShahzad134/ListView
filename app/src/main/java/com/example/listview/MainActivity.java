f4a1338c5dbf3266c5a52879972cbfab080b6424
Sameer Shahzad <bcsf19a030@pucit.edu.pk>
2023-01-22 14:53:59 +0500

    Arranging

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -6,12 +6,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //creating listView
         ListView listView = (ListView) findViewById(R.id.list_view);
         String[] values = new String[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
